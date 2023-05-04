185a02571872a6a2c0689ec4e144f5065e6d900e
VickyBondarenko <quikyv@gmail.com>
2023-05-05 00:18:55 +0200

    Add patch route

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -1,15 +1,15 @@
-const contacts = require("../models/contacts");
+const { Contact } = require("../models/contact");
 const { ctrlWrapper } = require("../helpers/");
 const { HttpError } = require("../helpers");
 
 const getAll = async (req, res) => {
-  const result = await contacts.listContacts();
+  const result = await Contact.find();
   res.json(result);
 };
 
 const getById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.getContactById(contactId);
+  const result = await Contact.findById(contactId);
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -17,13 +17,13 @@ const getById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const result = await contacts.addContact(req.body);
+  const result = await Contact.create(req.body);
   res.status(201).json(result);
 };
 
 const deleteByID = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndDelete(contactId);
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -32,7 +32,23 @@ const deleteByID = async (req, res) => {
 
 const updateById = async (req, res) => {
   const { contactId } = req.params;
-  const result = await contacts.updateContact(contactId, req.body);
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+  if (!result) {
+    throw HttpError(404, `Not found`);
+  }
+  res.json(result);
+};
+
+const updateFavoritContact = async (req, res) => {
+  const { contactId } = req.params;
+  if (!req.body) {
+    throw HttpError(404, `missing field favorite`);
+  }
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
   if (!result) {
     throw HttpError(404, `Not found`);
   }
@@ -45,4 +61,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   deleteByID: ctrlWrapper(deleteByID),
   updateById: ctrlWrapper(updateById),
+  updateFavoritContact: ctrlWrapper(updateFavoritContact),
 };
---
+++ models/contact.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require("mongoose");
+const handleMongooseError = require("../utils/handleMongooseError");
+const Joi = require("joi");
+
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+contactSchema.post("save", handleMongooseError);
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required().messages({
+    "any.required": `missing fields`,
+    "string.base": `missing fields`,
+  }),
+  email: Joi.string().email({ minDomainSegments: 2 }).required(),
+  phone: Joi.string().required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoritMovie = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "any.required": `missing field favorite`,
+  }),
+});
+const Contact = model("contact", contactSchema);
+const schemas = {
+  contactAddSchema,
+  updateFavoritMovie,
+};
+
+module.exports = { Contact, schemas };
