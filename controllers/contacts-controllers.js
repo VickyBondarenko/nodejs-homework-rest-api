5d58f99cf71b2664e0bf53d09bc2a0c89e9f4a08
VickyBondarenko <quikyv@gmail.com>
2023-05-11 12:24:46 +0200

    add filter by favorit

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -3,8 +3,20 @@ const { ctrlWrapper, HttpError } = require("../helpers/");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, favorite } = req.query;
   const skip = (page - 1) * limit;
+  console.log("favorite", favorite);
+  if (favorite) {
+    const result = await Contact.find(
+      { owner, favorite },
+      "-createdAt -updatedAt",
+      {
+        skip,
+        limit,
+      }
+    ).populate("owner", "name email");
+    res.json(result);
+  }
   const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
     skip,
     limit,
